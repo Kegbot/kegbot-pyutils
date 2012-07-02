06ca926401f96b7b9b5be22062870c21d3873bc0
mike wakerly <opensource@hoho.com>
2012-07-01 17:30:33 -0700

    Initial commit.
    Sources branched from main Kegbot git repository:
      URL: https://github.com/Kegbot/kegbot
      Commit: b8c46fda9140138c8c2e73f81f2798a9a08183be

---
+++ README.md
@@ -0,0 +1,15 @@
+# kegbot-pyutils
+
+This is a small collection of common Python library code used in the Kegbot
+project.
+
+Homepage: http://github.com/Kegbot/kegbot-pyutils
+
+## License and Copyright
+
+All code is offered under the **GPLv2** license, unless otherwise noted.  Please
+see ``LICENSE.txt`` for the full license.
+
+All code and documentation are **Copyright 2003-2011 Mike Wakerly**, unless
+otherwise noted.
+
---
+++ setup.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+"""Library of miscellaneous utilities used within Kegbot.
+"""
+
+VERSION = '0.1'
+DOCLINES = __doc__.split('\n')
+SHORT_DESCRIPTION = DOCLINES[0]
+LONG_DESCRIPTION = '\n'.join(DOCLINES[2:])
+
+def setup_package():
+  from distribute_setup import use_setuptools
+  use_setuptools()
+  from setuptools import setup, find_packages
+
+  setup(
+      name = 'kegbot-pyutils',
+      version = VERSION,
+      description = SHORT_DESCRIPTION,
+      long_description = LONG_DESCRIPTION,
+      author = 'mike wakerly',
+      author_email = 'opensource@hoho.com',
+      url = 'http://kegbot.org/',
+      packages = find_packages(exclude=['testdata']),
+      namespace_packages = ['kegbot'],
+      scripts = [
+        'distribute_setup.py',
+      ],
+      install_requires = [
+        'python-gflags >= 1.8',
+      ],
+      include_package_data = True,
+  )
+
+if __name__ == '__main__':
+  setup_package()
