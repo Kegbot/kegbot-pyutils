6c180e1718290d03ca8ca8ce02a8e6d1ee455b61
mike wakerly <opensource@hoho.com>
2020-04-29 08:22:26 -0400

    v1.0.0
    Closes #2

--- setup.py
+++ setup.py
@@ -1,33 +1,24 @@
-#!/usr/bin/env python
-
-"""Library of miscellaneous utilities used within Kegbot.
-"""
-
 from setuptools import setup, find_packages
+import pathlib
 
-VERSION = '0.1.8'
-DOCLINES = __doc__.split('\n')
-SHORT_DESCRIPTION = DOCLINES[0]
-LONG_DESCRIPTION = '\n'.join(DOCLINES[2:])
+VERSION = '1.0.0'
+BASE_DIR = pathlib.Path(__file__).parent
+README = (BASE_DIR / 'README.md').read_text()
 
-def setup_package():
-
-  setup(
-      name = 'kegbot-pyutils',
-      version = VERSION,
-      description = SHORT_DESCRIPTION,
-      long_description = LONG_DESCRIPTION,
-      author = 'mike wakerly',
-      author_email = 'opensource@hoho.com',
-      url = 'http://kegbot.org/',
-      packages = find_packages(exclude=['testdata']),
-      namespace_packages = ['kegbot'],
-      install_requires = [
-        'isodate',
-        'python-gflags >= 1.8',
-      ],
-      include_package_data = True,
-  )
-
-if __name__ == '__main__':
-  setup_package()
+setup(
+  name='kegbot-pyutils',
+  version=VERSION,
+  description='Python utilities used in the Kegbot Project',
+  long_description=README,
+  long_description_content_type='text/markdown',
+  author='The Kegbot Project Contributors',
+  author_email='info@kegbot.org',
+  license='MIT',
+  url='https://kegbot.org/',
+  packages=find_packages(exclude=['testdata']),
+  namespace_packages=['kegbot'],
+  install_requires=[
+    'python-gflags >= 1.8',
+  ],
+  include_package_data=True,
+)
