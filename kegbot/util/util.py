512111303d85a5781f9f9fefb9a281e37d953db2
mike wakerly <opensource@hoho.com>
2020-04-28 21:58:13 -0400

    py2to3: remove unnecessary libs

--- kegbot/util/util.py
+++ kegbot/util/util.py
@@ -18,98 +18,59 @@
 
 """General purpose utilities, bits, and bobs"""
 
-from past.builtins import cmp
-from builtins import str
-from builtins import object
-from future.utils import raise_
 import asyncore
-import datetime
+import collections
 import errno
 import os
 import sys
-import types
 import threading
 import time
 import traceback
 import logging
-from future.utils import with_metaclass
 
-### Misc classes
-def Enum(*defs):
-  """http://code.activestate.com/recipes/413486/"""
-  assert defs, "Empty enums are not supported"
 
-  names = []
-  namedict = {}
-  idx = 0
-  for item in defs:
-    if type(item) == tuple:
-      name, idx = item
-    else:
-      name = item
-    namedict[idx] = name
-    names.append(name)
-    idx += 1
+class Field:
+  """Base field type for use with DeclarativeMetaclass."""
 
-  class EnumClass(object):
-    __slots__ = names
-    def __iter__(self):        return iter(constants)
-    def __len__(self):         return len(constants)
-    def __getitem__(self, i):  return constants[i]
-    def __repr__(self):        return 'Enum' + str(names)
-    def __str__(self):         return 'enum ' + str(constants)
-    def get_names(self):       return names
 
-  class EnumValue(object):
-    __slots__ = ('__value')
-    def __init__(self, value): self.__value = value
-    Value = property(lambda self: self.__value)
-    EnumType = property(lambda self: EnumType)
-    def __hash__(self):        return hash(self.__value)
-    def __cmp__(self, other):
-      # C fans might want to remove the following assertion
-      # to make all enums comparable by ordinal value {;))
-      assert self.EnumType is other.EnumType, "Only values from the same enum are comparable"
-      return cmp(self.__value, other.__value)
-    def __invert__(self):      return constants[maximum - self.__value]
-    def __bool__(self):     return bool(self.__value)
-    def __repr__(self):        return str(namedict[self.__value])
+class DeclarativeMetaclass(type):
+  """Collect Fields declared on the base classes, and exposes as `.fields`.
+  
+  Borrowed from django.forms.ModelForm.
+  """
+  def __new__(mcs, name, bases, attrs):
+    fields = []
+    for key, value in list(attrs.items()):
+      if isinstance(value, Field):
+        fields.append((key, value))
+        attrs.pop(key)
+    attrs['fields'] = collections.OrderedDict(fields)
+    new_class = super(DeclarativeMetaclass, mcs).__new__(mcs, name, bases, attrs)
+    return new_class
 
-  maximum = len(names) - 1
-  constants = {}
-  i = 0
-  for item in defs:
-    if type(item) == tuple:
-      name, idx = item
-    else:
-      name, idx = item, i
-    assert idx not in constants
-    i = idx + 1
-    val = EnumValue(idx)
-    setattr(EnumClass, name, val)
-    constants[idx] = val
-  EnumType = EnumClass()
-  return EnumType
+  @classmethod
+  def __prepare__(metacls, name, bases, **kwds):
+    return collections.OrderedDict()
 
 
 class KegbotThread(threading.Thread):
-  """ Convenience wrapper around a threading.Thread """
+  """Convenience wrapper around a threading.Thread"""
   def __init__(self, name):
     threading.Thread.__init__(self)
     self.setName(name)
     self.setDaemon(True)
     self._quit = False
     self._logger = logging.getLogger(self.getName())
-    self._started = False
+    self._thread_started = False
 
   def hasStarted(self):
-    return self._started
+    return self._thread_started
 
   def Quit(self):
     self._quit = True
 
   def run(self):
-    self._started = True
+    self._thread_started = True
     try:
       self.ThreadMain()
     except:
@@ -132,140 +93,6 @@ class AsyncoreThread(KegbotThread):
         time.sleep(0.5)
     self._logger.info('Quitting')
 
-
-class AttrDict(dict):
-  def __setattr__(self, name, value):
-    self.__setitem__(name, value)
-
-  def __getattr__(self, name):
-    try:
-      return self.__getitem__(name)
-    except KeyError:
-      raise_(AttributeError, 'No attribute named %s' % name)
-
-
-class SimpleGraph(object):
-  """Inspired by http://www.python.org/doc/essays/graphs.html"""
-  def __init__(self, vertices):
-    """Build up a graph with unidirectional vertices"""
-    self._graph = {}
-    for a, b in vertices:
-      if a in self._graph:
-        self._graph[a].add(b)
-      else:
-        self._graph[a] = set((b,))
-
-  def ShortestPath(self, start, end, path=[]):
-    path = path + [start]
-    if start == end:
-      return path
-    if start not in self._graph:
-      return None
-    shortest = None
-    for node in self._graph[start]:
-      if node in path:
-        continue
-      newpath = self.ShortestPath(node, end, path)
-      if newpath:
-        if not shortest or len(newpath) < len(shortest):
-          shortest = newpath
-    return shortest
-
-
-# These metaclasses are modeled after Ian Bicking's examples:
-# http://blog.ianbicking.org/a-conservative-metaclass.html
-
-class DeclarativeMeta(type):
-  def __new__(meta, class_name, bases, new_attrs):
-    cls = type.__new__(meta, class_name, bases, new_attrs)
-    cls.__classinit__.__func__(cls, new_attrs)
-    return cls
-
-
-class Declarative(with_metaclass(DeclarativeMeta, object)):
-  def __classinit__(cls, new_attrs):
-    pass
-
-
-class BaseField(object):
-  def _Validate(self, value):
-    pass
-
-  def ParseValue(self, value):
-    self._Validate(value)
-    return value
-
-  def ToString(self, value):
-    return str(value)
-
-
-class BaseMessage(Declarative):
-  class_fields = {}
-
-  def __classinit__(cls, new_attrs):
-    cls.class_fields = cls.class_fields.copy()
-    for name, value in list(new_attrs.items()):
-      if isinstance(value, BaseField):
-        cls.add_field(name, value)
-
-  @classmethod
-  def add_field(cls, name, field):
-    cls.class_fields[name] = field
-    field.name = name
-    def getter(self, name=name):
-      return self._values[name]
-    def setter(self, value, name=name):
-      parser = self._fields[name]
-      self._values[name] = parser.ParseValue(value)
-    setattr(cls, name, property(getter, setter))
-
-  def __init__(self, initial=None, **kwargs):
-    self._fields = self.class_fields.copy()
-    self._values = dict((k, None) for k in list(self.class_fields.keys()))
-
-    if initial is not None:
-      self._UpdateFromDict(initial)
-    else:
-      self._UpdateFromDict(kwargs)
-
-  def __str__(self):
-    clsname = self.__class__.__name__
-    vallist = []
-    for fieldname, fieldvalue in self._values.items():
-      field = self._fields[fieldname]
-      vallist.append('%s=%s' % (fieldname, field.ToString(fieldvalue)))
-    valstr = (' '.join(vallist))
-    return '<%s: %s>' % (clsname, valstr)
-
-  def __iter__(self):
-    for name, field in list(self._GetFields().items()):
-      yield field
-
-  def __cmp__(self, other):
-    if not other or type(other) != type(self):
-      return -1
-    return cmp(self._values, other._values)
-
-  def _GetFields(self):
-    return self._fields
-
-  def _UpdateFromDict(self, d):
-    for k, v in d.items():
-      setattr(self, k, v)
-
-  def AsDict(self):
-    ret = {}
-    for k, v in self._fields.items():
-      ret[k] = self._values.get(k)
-    return ret
-
-  def SetValue(self, fieldname, value):
-    if fieldname not in self._values:
-      raise KeyError
-    self._values[fieldname] = value
-
-
-### Misc functions
 
 def daemonize():
   # Fork once
--- kegbot/util/util_test.py
+++ kegbot/util/util_test.py
@@ -1,50 +1,14 @@
 #!/usr/bin/env python
 
 """Unittest for util module"""
-from __future__ import print_function
 
-from builtins import str
 import unittest
 from . import util
-
-class EnumTestCase(unittest.TestCase):
-  def setUp(self):
-    self.enum = util.Enum(*(
-      'Foo',
-      ('Bar', 3),
-      'Dip',
-      ('Baz', 10),
-    ))
-
-  def testBasicUse(self):
-    self.assertNotEqual(self.enum.Foo, self.enum.Bar)
-    print(str(self.enum))
-    self.assertEqual(self.enum.Foo.Value, 0)
-    self.assertEqual(self.enum.Bar.Value, 3)
-    self.assertEqual(self.enum.Dip.Value, 4)
-    self.assertEqual(self.enum.Baz.Value, 10)
-    self.assertEqual(str(self.enum.Bar), 'Bar')
-
-
-class SimpleGraphTestCase(unittest.TestCase):
-  def setUp(self):
-    self.graph = util.SimpleGraph((
-        ('a', 'b'),
-        ('a', 'c'),
-        ('c', 'd'),
-        ('d', 'e'),
-        ('c', 'e'),
-        ('e', 'f'),
-    ))
-
-  def testReverseMappings(self):
-    self.assertEquals(['a', 'c'], self.graph.ShortestPath('a', 'c'))
-    self.assertEquals(['a', 'c', 'e', 'f'], self.graph.ShortestPath('a', 'f'))
 
 class MiscTestCase(unittest.TestCase):
   def testGetVersion(self):
     version = util.get_version('bogus', 'foo')
-    self.assertEquals('foo', version)
+    self.assertEqual('foo', version)
 
 if __name__ == '__main__':
   unittest.main()
